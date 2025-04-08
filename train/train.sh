ff564c12a9c7f86976acc6ea67a1b267b8df459a
mistletoe235 <92664458+mistletoe235@users.noreply.github.com>
2025-04-08 10:05:29 +0800

    Update train.sh

--- train/train.sh
+++ train/train.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-torchrun --standalone --nnodes 1 --nproc-per-node 8 /cpfs01/shared/optimal/gaoyunpeng/openfly/train.py \
+torchrun --standalone --nnodes 1 --nproc-per-node 8 train.py \
   --grid_size 16 \
   --history_frames 2 \
   --pretrained_checkpoint YOUR_CHECKPOINT_PATH \
