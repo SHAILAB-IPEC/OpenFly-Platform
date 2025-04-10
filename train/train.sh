3170445cd63068bf9d485d3305ac90fbf8e435e4
Junli Liu <85859541+lxiaohaung@users.noreply.github.com>
2025-04-10 21:18:38 +0800

    Update train.sh

--- train/train.sh
+++ train/train.sh
@@ -4,12 +4,4 @@ torchrun --standalone --nnodes 1 --nproc-per-node 8 train.py \
   --grid_size 16 \
   --history_frames 2 \
   --pretrained_checkpoint YOUR_CHECKPOINT_PATH \
-  --output_dir YOUR_OUTPUT_PATH \
-  --batch_size 8 \
-  --num_workers 8 \
-  --num_epochs 100 \
-  --learning_rate 0.0001 \
-  --lr_decay_rate 0.9 \
-  --lr_decay_steps 5000 \
-  --weight_decay 0.0001 \
-  --num_checkpoints 10 \
+
